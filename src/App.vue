3f55490120ec276cd95b904071143838d9fbac4d
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-01-08 12:42:23 -0700

    Set up App to import Search and add title

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h2>Meditative Art of the Day</h2>
+    <h6>From the Harvard Art Museum</h6>
+    <Search/>
+  </div>
+</template>
+
+<script>
+import Search from './components/Search'
+
+export default {
+  name: 'App',
+  components: {
+    Search
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h2 {
+  font-size: 30px;
+  margin-bottom: 5px;
+}
+</style>
