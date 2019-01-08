3baa06e48a572dc877f118199aa68f099774699d
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-01-08 16:46:21 -0700

    Add carousel for photos with buttons for previous and next to cycle through

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="search">
-    <div v-for="artRecord in artRecords">
-      <img v-bind:src="artRecord.primaryimageurl" />
-      <h3 v-for="person in artRecord.people">{{person.name}}</h3>
-      <h3>{{artRecord.title}}</h3>
-      <h4>{{artRecord.dated}}</h4>
-      <h4>{{artRecord.displayName}}</h4>
-      <h4>{{artRecord.culture}}</h4>
+    <div class='carousel-controls'>
+      <button class='carousel-controls__button' @click="previous">prev</button>
+      <button class='carousel-controls__button' @click="next">next</button>
     </div>
+    <transition-group
+      class='carousel'
+    >
+      <div 
+        v-for="artRecord in artRecords"
+        class="image"
+        :key="artRecord.id"
+        tag="div"
+      >
+        <img v-bind:src="artRecord.primaryimageurl" />
+        <h3 v-for="person in artRecord.people">{{person.name}}</h3>
+        <h3>{{artRecord.title}}</h3>
+        <h4>{{artRecord.dated}}</h4>
+        <h4>{{artRecord.displayName}}</h4>
+        <h4>{{artRecord.culture}}</h4>
+      </div>
+    </transition-group>
+
   </div>
 </template>
 
 <script>
-import config from '../utilities/config.js'
+import config from '../utilities/config.js';
 
 export default {
   name: 'search',
@@ -23,7 +37,14 @@ export default {
     }
   },
   methods: {
-
+    next () {
+      const first = this.artRecords.shift()
+      this.artRecords = this.artRecords.concat(first)
+    },
+    previous () {
+      const last = this.artRecords.pop()
+      this.artRecords = [last].concat(this.artRecords)
+    }
   },
   mounted: function () {
     fetch(`https://api.harvardartmuseums.org/object?keyword=meditation&hasimage:0&apikey=${config.apiKey}`)
