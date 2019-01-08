c5686618658bbc1fc64a26540c0e0fa7e609bd5c
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-01-08 13:15:22 -0700

    Make successful fetch call to Harvard Art api

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,13 +5,23 @@
 </template>
 
 <script>
+import config from '../utilities/config.js'
+
 export default {
-  name: 'search',
-  data () {
-    return {
-      msg: 'Search',
-    }
-  }
+ name: 'search',
+ data () {
+   return {
+     msg: 'Search'
+   }
+ },
+ methods: {
+
+ },
+ mounted: function () {
+   fetch(`https://api.harvardartmuseums.org/object?keyword=meditation&hasimage:0&apikey=${config.apiKey}`)
+     .then(response => response.json())
+     .then(results => console.log(results.records));
+ }
 }
 </script>
 
