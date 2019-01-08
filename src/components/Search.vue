542173294a58ea2e9dffd79de790377d89167757
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-01-08 14:02:15 -0700

    Save fetch call to state and render images to the dom

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="search">
-
+    <div v-for="artRecord in artRecords">
+      <img v-bind:src="artRecord.primaryimageurl" />
+    </div>
   </div>
 </template>
 
@@ -8,20 +10,24 @@
 import config from '../utilities/config.js'
 
 export default {
- name: 'search',
- data () {
-   return {
-     msg: 'Search'
-   }
- },
- methods: {
+  name: 'search',
+  data () {
+    return {
+      msg: 'Search',
+      artRecords: ''
+    }
+  },
+  methods: {
 
- },
- mounted: function () {
-   fetch(`https://api.harvardartmuseums.org/object?keyword=meditation&hasimage:0&apikey=${config.apiKey}`)
-     .then(response => response.json())
-     .then(results => console.log(results.records));
- }
+  },
+  mounted: function () {
+    fetch(`https://api.harvardartmuseums.org/object?keyword=meditation&hasimage:0&apikey=${config.apiKey}`)
+      .then(response => response.json())
+      .then(results => {
+        this.artRecords = results.records
+        console.log(this.artRecords)
+      })  
+  }
 }
 </script>
 
